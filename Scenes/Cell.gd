bfa4ee881968e46f5d1ca83560f58093c6e52688
Joseph Burke <jjburke011@gmail.com>
2022-11-10 23:33:15 -0600

    stuff

--- Scenes/Cell.gd
+++ Scenes/Cell.gd
@@ -1,14 +1,29 @@
 extends KinematicBody2D
 
 onready var sprite = $Sprite
+onready var timer = $HitFlash
+onready var timer1 = $KnockBack
 var health = 10
+var knock_back_by_damage = Vector2.ZERO
 
 func _process(delta):
 	if(health <= 0):
 		queue_free()
+func _physics_process(delta):
+	if (!timer1.is_stopped()):
+		move_and_slide(knock_back_by_damage)
+	pass
 func _take_damage(dmg):
 	health -= dmg
+	timer.start()
 	sprite.modulate = Color.red
-	yield(get_tree().create_timer(.2),"timeout")
-	sprite.modulate = Color.white
+func _get_knocked_back(knock_back):
+	knock_back_by_damage = knock_back
+	timer1.start()
 	
+func _on_Timer_timeout():
+	sprite.modulate = Color.white
+
+
+func _on_KnockBack_timeout():
+	timer1.stop()
--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -3,53 +3,63 @@ extends KinematicBody2D
 var speed = 300
 onready var animation_player = $AnimationPlayer
 onready var animation_tree = $AnimationTree
+onready var fist_collision = $Area2D/CollisionShape2D
+
 onready var animation_state = animation_tree.get("parameters/playback")
 var movement = Vector2.ZERO
 var blend = Vector2.ZERO
 var is_flying = true
 var damage = 2
+var knock_back = Vector2.ZERO
 
 func _physics_process(_delta):
 	if movement != Vector2.ZERO:
 		blend = movement.normalized()
 		animation_tree.set("parameters/Idle/blend_position", blend)
 		animation_tree.set("parameters/Walk/blend_position", blend)
+		animation_tree.set("parameters/Attacking/blend_position", blend)
 	_get_input()
 	
 func _get_input():
 	if(Input.is_action_pressed("i_up")):
+		print("Pressed W")
 		movement = Vector2(0,-1*speed)
 		animation_state.travel("Walk")
 		move_and_slide(movement)
 		
 	if(Input.is_action_pressed("i_down")):
+		print("Pressed S")
 		movement = Vector2(0,1*speed)
 		animation_state.travel("Walk")
 		move_and_slide(movement)
 		
 	if(Input.is_action_pressed("i_left")):
+		print("Pressed A")
 		movement = Vector2(-1*speed,0)
 		animation_state.travel("Walk")
 		move_and_slide(movement)
 		
 	if(Input.is_action_pressed("i_right")):
+		print("Pressed D")
 		movement = Vector2(1*speed,0)
 		animation_state.travel("Walk")
 		move_and_slide(movement)
-	if(!(Input.is_action_pressed("i_up") || Input.is_action_pressed("i_down") || Input.is_action_pressed("i_left") || Input.is_action_pressed("i_right"))):
+		
+	if(!(Input.is_action_pressed("i_up") || Input.is_action_pressed("i_down") || Input.is_action_pressed("i_left") || Input.is_action_pressed("i_right") || Input.is_action_pressed("i_punch"))):
 		animation_state.travel("Idle")
+		
 	if(Input.is_action_just_pressed("i_toggle_flight")):
+		print("Pressed R")
 		is_flying = !is_flying
 		set_collision_mask_bit(1,is_flying)
 		set_collision_mask_bit(0,is_flying)
+		
 	if(Input.is_action_just_pressed("i_punch")):
-		if (blend != Vector2.ZERO):
-			if (blend == Vector2(1,0)):
-				animation_player.play("attack_right")
-			elif(blend == Vector2(-1,0)):
-				animation_player.play("attack_left")
-	
-
+		print("Pressed SPACE")
+		knock_back = blend * 1000 * damage
+		animation_state.travel("Attacking")
+		
 func _on_Area2D_body_entered(body):
 	if (body.is_in_group("enemy")):
 		body._take_damage(damage)
+		body._get_knocked_back(knock_back)
